149b1eddb581d9f273a2c11e82e72a96f0f44580
Kevin <kevin12kim27@gmail.com>
2021-07-06 13:29:29 -0700

    CalFI properly documented and works

--- fpmcode/WallerCode/WallerCalibration.py
+++ fpmcode/WallerCode/WallerCalibration.py
@@ -7,22 +7,26 @@ from calCoord import calCoord
 from calFI import calFI
 import scipy.io as sio
 fname = "/home/kevin/Harvey Mudd/biophotonics/Angle_SelfCalibration-master/data/LED_cheekCell_comp_misaligned_input.mat"
+fname2 = "/home/kevin/Harvey Mudd/biophotonics/code/data/input_data_USAF.mat"
 data_fname = os.path.normpath(fname)
+data_fname2 = os.path.normpath(fname2)
 data = sio.loadmat(data_fname,struct_as_record=False,squeeze_me=True)
+data2 = sio.loadmat(data_fname2)
 metadata = data['metadata']
 mag=metadata.objective.mag*metadata.objective.system_mag
 NA = metadata.objective.na
 dpix_c = metadata.camera.pixel_size_um;
 wavelength = metadata.illumination.wavelength_um;
 freqUV = metadata.source_list.na_design/wavelength
-print(freqUV.shape)
 I = data['data']
+#I = data2['imseqlow']
 imSz = len(I[:,:,0])
+
 freqXY, con, radP, xI, yI, uI, vI, XYmid = calCoord(freqUV, imSz, dpix_c,mag, NA, wavelength)
 sigmaG = 2
 avgFI2 = calFI(I,xI,yI,XYmid, radP,sigmaG)
 fig,ax = plt.subplots(1,1)
-him = ax.imshow(avgFI2,norm=colors.LogNorm())
+him = ax.imshow(np.abs(avgFI2[:,:,0]),norm=colors.LogNorm())
 plt.show()
 
 
--- fpmcode/WallerCode/calFI.py
+++ fpmcode/WallerCode/calFI.py
@@ -11,15 +11,23 @@ from scipy.ndimage.filters import gaussian_filter
 #radP: radius of the NA (in pixels)
 #sigmaG: sigma=2 within gaussian filtering
 
+"""Program steps
+    1) convert images into intensity valued fourier spectrum
+    2) take the mean across all spectrum images-- this results in a "mean spectrum"
+    3) take the mean value outside the 2NA support in the mean spectrum
+    4) from the mean spectrum, floor out the noise outside the 2NA support (any pixels outside 2NA support with values less than 3*mean Imag
+    5)  divide out the modified mean spectrum from all the FT images
+    6)  from those images, convolve them with Gaussian Kernel std.dev 2 pixels"""
+
 def calFI(I,xI, yI,XYmid, radP, sigmaG):
     FI = fftshift(fft2(I,axes=[1,0]),axes=[1,0]) #FT the image set
     avgFI = np.mean(np.abs(FI),2) #take the mean across all images
-    print(radP)
     w_2NA=np.sqrt((xI-XYmid[0])**2 + (yI-XYmid[1])**2)>2*radP; #Outside 2NA support
-    print(xI)
     mT = np.mean(w_2NA * avgFI) #the mean value outside the 2NA support
-    avgFI2 = np.where(avgFI < 3*mT, 3*mT, avgFI)#if values are less than 3*mT then floor them to the mean.
-    return avgFI2
+    avgFI2 = np.where(avgFI < 3*mT, 3*mT, avgFI)#if values are less than 3*mT then floor them to the 3*mT (essentially getting rid of noise outside the 2NA support).
+    FIdiv = FI/np.repeat(avgFI2[:,:,np.newaxis], FI.shape[2],axis=2) #dividing out by the floored average
+    FIdivG = gaussian_filter(np.abs(FIdiv), sigma=2) #convolving with the Gaussian Kernel with std.dev = 2 pixels
+    return FIdivG
     
 
 
